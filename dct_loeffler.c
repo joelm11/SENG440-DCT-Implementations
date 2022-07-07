40140b6dbe43faae7a5172be0547d7f202fc76b8
Joel Meuleman <joelmeuleman@Joels-MacBook-Pro.local>
2022-07-07 15:32:55 -0700

    Converted all doubles and runs now debugging

--- dct_loeffler.c
+++ dct_loeffler.c
@@ -4,13 +4,13 @@
 #include <math.h>
 
 
-void reflector(unsigned int *a, unsigned int *b) {
-    unsigned int temp_a = *a + *b;
+void reflector(double *a, double *b) {
+    double temp_a = *a + *b;
     // make sure the addition operation is saturated
     if (temp_a > 255) {
         temp_a = 255;
     }
-    unsigned int temp_b = *a - *b;
+    double temp_b = *a - *b;
     // make sure the subtraction operation is saturated
     if (temp_b  > 255) {
         temp_b = 0;
@@ -21,7 +21,7 @@ void reflector(unsigned int *a, unsigned int *b) {
 }
 
 // The Rotator function can be impemented with butterfly operations in hardware.
-void rotator(unsigned int *a, unsigned int *b, double k, unsigned int n) {
+void rotator(double *a, double *b, double k, double n) {
     double c = (n * M_PI) / 16;
     double temp_a = (*a * k * cos(c)) + (*b * k * sin(c));
     // make sure the addition operation is saturated
@@ -34,19 +34,19 @@ void rotator(unsigned int *a, unsigned int *b, double k, unsigned int n) {
         temp_b = 0;
     }
 
-    *a = (unsigned int) temp_a;
-    *b = (unsigned int) temp_b;
+    *a = temp_a;
+    *b = temp_b;
 }
 
 
 
 int main() {
     // the main input array for 1D-dct 
-    unsigned int x[] = {1,2,3,4,5,6,7,8};
+    double x[] = {1,2,3,4,5,6,7,8};
 
     // stage 1:
     int i;
-    for (i = 8; i; i--) {
+    for (i = 4; i; i--) {
         reflector(&x[8-i], &x[i-1]);
     }
 
@@ -65,15 +65,15 @@ int main() {
     // stage 4:
     reflector(&x[7], &x[4]);
     double temp = sqrt(2) * (double) x[5];
-    x[5] = (unsigned int) temp;
+    x[5] = temp;
     temp = sqrt(2) * (double) x[6];
-    x[6] = (unsigned int) temp;
+    x[6] = temp;
     
     //print result
     for (i = 8; i; i--) {
-        printf("x[%d]: %u\n", 8-i, x[8-i]);
+        printf("x[%d]: %.3f\t", 8-i, x[8-i]);
     }
-
+    printf("\n");
     // keep in mind that after loeffler's algorithm is done, the output array X has shifted:
     // x[0] = X[0], x[1] = X[4], x[2] = X[2], x[3] = X[6], x[4] = X[7], x[5] = X[3], x[6] = X[5], x[7] = X[1] 
 }
