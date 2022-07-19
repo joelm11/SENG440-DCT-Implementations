e0b559b5d389fbc06001ffcc175436b525bb822c
Joel Meuleman <j.meuleman11@gmail.com>
2022-07-18 20:22:09 -0400

    Began work on NEON implementation

--- dct_loeffler_vectorized.c
+++ dct_loeffler_vectorized.c
@@ -12,9 +12,9 @@
 */
 
 #include <stdio.h>
+#include <math.h>   
 #include <stdint.h>
-#include <math.h>  
-// #include "arm_neon.h"
+#include "arm_neon.h"
 
 // Pre-computed constants for the simplified rotator operation with SF = 2^5  
 // Ox[0] = (k = 1, C = 1), Ox[1] = (k = 1, C = 3), Ox[2] = (k = root(2), C = 6) 
@@ -52,69 +52,72 @@ int main() {
     
     // Assume 8-bit input to DCT (pixel value) 
     uint8_t x[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    short int s1_out[8] = {0};
-    short int s2_out[8] = {0};
-    short int s3_out[8] = {0};
-    short int s4_out[8] = {0};
 
-    // Stage 1: 
-    s1_out[0] = x[0] + x[7]; 
-    s1_out[7] = x[0] - x[7]; 
-    s1_out[1] = x[1] + x[6];
-    s1_out[6] = x[1] - x[6]; 
-    s1_out[2] = x[2] + x[5];
-    s1_out[5] = x[2] - x[5]; 
-    s1_out[3] = x[3] + x[4];
-    s1_out[4] = x[3] - x[4];
+    // STAGE 1:  
+    // Load values into NEON registers  
+    // Load first 4 values into one register 
+    // Load next 4 values into another register 
+    // Add them, then subtract them
+    uint8x8_t s1_start; 
+    s1_start = vld1_u8(x); 
+    // Execute reflectors
+    // s1_out[0] = x[0] + x[7]; 
+    // s1_out[7] = x[0] - x[7]; 
+    // s1_out[1] = x[1] + x[6];
+    // s1_out[6] = x[1] - x[6]; 
+    // s1_out[2] = x[2] + x[5];
+    // s1_out[5] = x[2] - x[5]; 
+    // s1_out[3] = x[3] + x[4];
+    // s1_out[4] = x[3] - x[4];
 
-    // Stage 2:
-    s2_out[0] = s1_out[0] + s1_out[3];
-    s2_out[3] = s1_out[0] - s1_out[3]; 
-    s2_out[1] = s1_out[1] + s1_out[2];
-    s2_out[2] = s1_out[1] - s1_out[2];
-    trotator(&s1_out[4], &s1_out[7], &s2_out[4], &s2_out[7], 1);
-    trotator(&s1_out[5], &s1_out[6], &s2_out[5], &s2_out[6], 0); 
+    // // Stage 2:
+    // s2_out[0] = s1_out[0] + s1_out[3];
+    // s2_out[3] = s1_out[0] - s1_out[3]; 
+    // s2_out[1] = s1_out[1] + s1_out[2];
+    // s2_out[2] = s1_out[1] - s1_out[2];
+    // trotator(&s1_out[4], &s1_out[7], &s2_out[4], &s2_out[7], 1);
+    // trotator(&s1_out[5], &s1_out[6], &s2_out[5], &s2_out[6], 0); 
 
-    // Check output of stage
-    printf("Stage 2 output\n");  
-    debug(s2_out);
+    // // Check output of stage
+    // printf("Stage 2 output\n");  
+    // debug(s2_out);
 
 
-    // Stage 3:
-    s3_out[0] = s2_out[0] + s2_out[1]; 
-    s3_out[1] = s2_out[0] - s2_out[1]; 
-    trotator(&s2_out[2], &s2_out[3], &s3_out[2], &s3_out[3], 2); 
-    s3_out[4] = s2_out[4] + s2_out[6];
-    s3_out[6] = s2_out[4] - s2_out[6];
-    s3_out[7] = s2_out[7] + s2_out[5];
-    s3_out[5] = s2_out[7] - s2_out[5];
+    // // Stage 3:
+    // s3_out[0] = s2_out[0] + s2_out[1]; 
+    // s3_out[1] = s2_out[0] - s2_out[1]; 
+    // trotator(&s2_out[2], &s2_out[3], &s3_out[2], &s3_out[3], 2); 
+    // s3_out[4] = s2_out[4] + s2_out[6];
+    // s3_out[6] = s2_out[4] - s2_out[6];
+    // s3_out[7] = s2_out[7] + s2_out[5];
+    // s3_out[5] = s2_out[7] - s2_out[5];
    
-    // Check output of stage
-    printf("Stage 3 output\n");  
-    debug(s3_out);
+    // // Check output of stage
+    // printf("Stage 3 output\n");  
+    // debug(s3_out);
 
-    // Stage 4:   
-    int temp = s3_out[7];
-    s3_out[7] = s3_out[7] + s3_out[4]; 
-    s3_out[4] = temp - s3_out[4];  
-    s3_out[5] *= sqrt(2);
-    s3_out[6] *= sqrt(2);  
+    // // Stage 4:   
+    // int temp = s3_out[7];
+    // s3_out[7] = s3_out[7] + s3_out[4]; 
+    // s3_out[4] = temp - s3_out[4];  
+    // s3_out[5] *= sqrt(2);
+    // s3_out[6] *= sqrt(2);  
 
-    // Check output of stage
-    printf("Stage 4 output\n");  
-    debug(s3_out);
+    // // Check output of stage
+    // printf("Stage 4 output\n");  
+    // debug(s3_out);
 
-    // Print final DCT product with attached final scale factor,
-    // prints reordered to match input sequence ordering 
-    printf("%.3f\t", s3_out[0] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[7] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[2] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[5] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[1] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[6] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[3] * 1 / (2 * sqrt(2)));
-    printf("%.3f\t", s3_out[4] * 1 / (2 * sqrt(2)));
-    printf("\n"); 
+    // // Print final DCT product with attached final scale factor,
+    // // prints reordered to match input sequence ordering 
+    // printf("%.3f\t", s3_out[0] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[7] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[2] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[5] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[1] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[6] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[3] * 1 / (2 * sqrt(2)));
+    // printf("%.3f\t", s3_out[4] * 1 / (2 * sqrt(2)));
+    // printf("\n"); 
 
     // keep in mind that after loeffler's algorithm is done, the output array X has shifted:
     // x[0] = X[0], x[1] = X[4], x[2] = X[2], x[3] = X[6], x[4] = X[7], x[5] = X[3], x[6] = X[5], x[7] = X[1] 
