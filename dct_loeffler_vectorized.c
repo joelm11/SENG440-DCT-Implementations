5ad385121fa5d0dce39b0d66c72b9d699e6e7b9f
Joel Meuleman <j.meuleman11@gmail.com>
2022-07-21 17:02:19 -0400

    Working LDCT entirely within NEON registers

--- dct_loeffler_vectorized.c
+++ dct_loeffler_vectorized.c
@@ -27,11 +27,6 @@ short int C[] = {31, 27, 17};
 // Here the simplified rotator function has been implemented with reduced multiplications
 void trotator(int16_t *ain, int16_t *bin, int16_t *aout, int16_t *bout, int k) 
 { 
-    // DEBUG 
-    printf("%d\n", *ain); 
-    printf("%d\n", *bin); 
-    printf("%d\n", *aout);
-    printf("%d\n", *bout);
 
     short int o1, o2, c_temp; 
 
@@ -56,6 +51,11 @@ void debug(int16x4_t data)
     printf("%d\t", result);
 };
 
+void test(int16_t *temp) 
+{ 
+
+    printf("Passed value: %d\n", *temp);
+};
 
 
 //  TODO: Implement saturating arithmetic (for all stages)
@@ -76,13 +76,10 @@ int main()
     
     // Load first 4 inputs to NEON register
     top_coeffs = vld1_s16(x1); 
-    
     // Load last 4 inputs to NEON register
     bot_coeffs = vld1_s16(x2); 
-    
     // Reverse to make reflector operation convenient
     bot_coeffs = vrev64_s16(bot_coeffs); 
-
     // Add vectors and store result in larger intermediate vector
     s1_out_top = vadd_s16(top_coeffs, bot_coeffs);
     // Note that these are now reversed in order within the vector
@@ -102,79 +99,64 @@ int main()
     s2_out_top[3] = s1_out_top[0] - s1_out_top[3];
     s2_out_top[1] = s1_out_top[1] + s1_out_top[2];
     s2_out_top[2] = s1_out_top[1] - s1_out_top[2];
-    
-    // TODO: Weird bug currently where after I run the rotators it affects 
-    // the values in s2_out_top even though only accessing bot lol 
-    // some funniness with rotator access i guess
-    debug(s2_out_top);  
-    debug(s1_out_bot);
-    printf("\n"); 
 
-    // Bottom coefficients
-    trotator(&s1_out_bot[4], &s1_out_bot[7], &s2_out_bot[4], &s2_out_bot[7], 1);
-    trotator(&s1_out_bot[5], &s1_out_bot[6], &s2_out_bot[5], &s2_out_bot[6], 0); 
-    
-    // int16_t result, temp;   
-    // result = vget_lane_s16(s1_out_top, 0);
-    // printf("%d\t", result);  
-    // temp = vget_lane_s16(s1_out_top, 3);
-    // printf("%d\t", result);   
+    // Bottom coefficients (Keep in mind bot array input in reversed order)
+    trotator(&s1_out_bot[3], &s1_out_bot[0], &s2_out_bot[0], &s2_out_bot[3], 1);
+    trotator(&s1_out_bot[2], &s1_out_bot[1], &s2_out_bot[1], &s2_out_bot[2], 0);   
 
-    // // result = vget_lane_s16(s2_out_top, 0);
-    // printf("%d\t", temp + result); 
     // DEBUG  
     printf("Stage 2:\n");
     debug(s2_out_top); 
     debug(s2_out_bot);
     printf("\n");
 
-    // /*  STAGE 3 */ 
-    // // Top coefficients 
-    // int16x4_t s3_out_top, s3_out_bot;
-    // s3_out_top[0] = s2_out_top[0] + s2_out_top[1]; 
-    // s3_out_top[1] = s2_out_top[0] - s2_out_top[1];  
-    // // Rotator implementation as a function, will likely greatly benefit from assembly inlining 
-    // // Addition of c_temp can be vectorized 
-    // // All other operations are scalar 
-    // // VMLA VMLA_LANE VMUL_N
-    // trotator(&s2_out_top[2], &s2_out_top[3], &s3_out_top[2], &s3_out_top[3], 2);   
+    /*  STAGE 3 */ 
+    // Top coefficients 
+    int16x4_t s3_out_top, s3_out_bot;
+    s3_out_top[0] = s2_out_top[0] + s2_out_top[1]; 
+    s3_out_top[1] = s2_out_top[0] - s2_out_top[1];  
+    // Rotator implementation as a function, will likely greatly benefit from assembly inlining 
+    // Addition of c_temp can be vectorized 
+    // All other operations are scalar 
+    // VMLA VMLA_LANE VMUL_N
+    trotator(&s2_out_top[2], &s2_out_top[3], &s3_out_top[2], &s3_out_top[3], 2);   
 
-    // // Bottom coefficients
-    // s3_out_bot[4] = s2_out_bot[4] + s2_out_bot[6];
-    // s3_out_bot[6] = s2_out_bot[4] - s2_out_bot[6];
-    // s3_out_bot[7] = s2_out_bot[7] + s2_out_bot[5];
-    // s3_out_bot[5] = s2_out_bot[7] - s2_out_bot[5];
+    // Bottom coefficients
+    s3_out_bot[0] = s2_out_bot[0] + s2_out_bot[2];
+    s3_out_bot[2] = s2_out_bot[0] - s2_out_bot[2];
+    s3_out_bot[3] = s2_out_bot[3] + s2_out_bot[1];
+    s3_out_bot[1] = s2_out_bot[3] - s2_out_bot[1];
    
-    // // DEBUG
-    // printf("Stage 3:\n"); 
-    // debug(s3_out_top); 
-    // debug(s3_out_bot);
-    // printf("\n");
+    // DEBUG
+    printf("Stage 3:\n"); 
+    debug(s3_out_top); 
+    debug(s3_out_bot);
+    printf("\n");
 
-    // /* STAGE 4: */  
-    // // int temp = s3_out[7];
-    // // s3_out[7] = s3_out[7] + s3_out[4]; 
-    // // s3_out[4] = temp - s3_out[4];  
-    // // s3_out[5] *= sqrt(2);
-    // // s3_out[6] *= sqrt(2);  
+    /* STAGE 4: */  
+    int16_t temp = s3_out_bot[3];
+    s3_out_bot[3] = s3_out_bot[3] + s3_out_bot[0]; 
+    s3_out_bot[0] = temp - s3_out_bot[0];  
+    s3_out_bot[1] *= sqrt(2);
+    s3_out_bot[2] *= sqrt(2);  
 
-    // // Check output of stage
-    // printf("Stage 4:\n"); 
-    // debug(s3_out_top); 
-    // debug(s3_out_bot);
-    // printf("\n");
+    // Check output of stage
+    printf("Stage 4:\n"); 
+    debug(s3_out_top); 
+    debug(s3_out_bot);
+    printf("\n");
 
-    // // Print final DCT product with attached final scale factor,
-    // // prints reordered to match input sequence ordering 
-    // printf("%.3f\t", s3_out[0] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[7] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[2] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[5] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[1] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[6] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[3] * 1 / (2 * sqrt(2)));
-    // printf("%.3f\t", s3_out[4] * 1 / (2 * sqrt(2)));
-    // printf("\n"); 
+    // Print final DCT product with attached final scale factor,
+    // prints reordered to match input sequence ordering 
+    printf("%.3f\t", s3_out_top[0] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_bot[3] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_top[2] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_bot[1] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_top[1] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_bot[2] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_top[3] * 1 / (2 * sqrt(2)));
+    printf("%.3f\t", s3_out_bot[0] * 1 / (2 * sqrt(2)));
+    printf("\n"); 
 
     // keep in mind that after loeffler's algorithm is done, the output array X has shifted:
     // x[0] = X[0], x[1] = X[4], x[2] = X[2], x[3] = X[6], x[4] = X[7], x[5] = X[3], x[6] = X[5], x[7] = X[1] 
