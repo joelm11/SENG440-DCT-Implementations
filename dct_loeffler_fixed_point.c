aa87ff635dc192672d715b08e547374913c9a20b
Cole Lutz <colelutz@uvic.ca>
2022-08-05 12:24:40 -0700

    fixed errors with dct_loeffler_fixed_point.c

--- dct_loeffler_fixed_point.c
+++ dct_loeffler_fixed_point.c
@@ -1,68 +1,57 @@
-// All cos and sqrt operations are pre-computed with scale-factors of 2^7 for better optimization
+// All cos and sqrt operations are pre-computed with scale-factors of 2^8 for better optimization
 #include <stdio.h>
-// pre-computed values for cos with a scale-factor of 2^7
-unsigned int c[] = {106, 126, 49};
-// pre-computed values for sin with a scale-factor of 2^7
-unsigned int s[] = {71, 25, 118};
-// pre-computed sqrt value with a scale-factor of 2^7
-unsigned int q[] = {128, 181};
+// pre-computed values for cos with a scale-factor of 2^8
+int c[] = {213, 251, 98};
+// pre-computed values for sin with a scale-factor of 2^8
+int s[] = {142, 50, 237};
+// pre-computed sqrt value with a scale-factor of 2^8
+int q[] = {256, 362};
 
+// swap the values of the provided ints
+void swap(int *a, int *b, int *c) {
+    int temp = *b;
+    *b = *a;
+    *a = *c;
+    *c = temp;
+}
 
-void reflector(unsigned int *a, unsigned int *b) {
+void reflector(int *a, int *b) {
     
-    unsigned int temp_a = *a + *b;
-    // make sure the addition operation is saturated
-    if (temp_a > 255) {
-        temp_a = 255;
-    }
+    int temp_a = *a + *b;
 
-    unsigned int temp_b = *a - *b;
-    // make sure the subtraction operation is saturated
-    if (temp_b  > 255) {
-        temp_b = 0;
-    }
+    int temp_b = *a - *b;
 
     *a = temp_a;
     *b = temp_b;
 }
 
 // The Rotator function can be impemented with butterfly operations in hardware.
-void rotator(unsigned int *a, unsigned int *b, unsigned int k, unsigned int n) {
+void rotator(int *a, int *b, int k, int n) {
     
-    unsigned int temp_a = ((*a << 7) * q[k] * c[n]) + ((*b << 7) * q[k] * s[n]);
-    // make sure the addition operation is saturated
-    // 534773760 is 255 * 2^21 which is the current scale factor at this point
-    if (temp_a > 534773760) {
-        temp_a = 534773760;
-    }
+    int temp_a = (*a * q[k] * c[n]) + (*b * q[k] * s[n]);
 
-    unsigned int temp_b = ((*b << 7) * q[k] * c[n]) - ((*a << 7) * q[k] * s[n]);
-    // make sure the subtraction operation is saturated 
-    // 534773760 is 255 * 2^21 which is the current scale factor at this point
-    if (temp_b > 534773760) {
-        temp_b = 0;
-    }
+    int temp_b = (*b * q[k] * c[n]) - (*a * q[k] * s[n]);
     
-    *a = (temp_a >> 21);
-    *b = (temp_b >> 21);
+    *a = temp_a;
+    *b = temp_b;
 }
 
 
 
 int main() {
     // the main input array for 1D-dct 
-    unsigned int x[] = {32,42,57,77,45,22,56,97};
+    int x[] = {1,2,3,4,5,6,7,8};
     
     // stage 1:
     int i;
     for (i = 4; i; i--) {
         reflector(&x[4-i], &x[3+i]);
-    }
+     }
 
     // print result
     printf("Stage 1:\n\n");
     for (i = 8; i; i--) {
-        printf("x[%d]: %u\n", 8-i, x[8-i]);
+        printf("x[%d]: %d\n", 8-i, x[8-i]);
     }
 
     // stage 2:
@@ -74,7 +63,7 @@ int main() {
     // print result
     printf("Stage 2:\n\n");
     for (i = 8; i; i--) {
-        printf("x[%d]: %u\n", 8-i, x[8-i]);
+        printf("x[%d]: %d\n", 8-i, x[8-i]);
     }
 
     // stage 3:
@@ -86,32 +75,27 @@ int main() {
     // print result
     printf("Stage 3:\n\n");
     for (i = 8; i; i--) {
-        printf("x[%d]: %u\n", 8-i, x[8-i]);
+        printf("x[%d]: %d\n", 8-i, x[8-i]);
     }
 
     // stage 4:
     reflector(&x[7], &x[4]);
-    unsigned int temp = q[1] * (x[5] << 7);
-    // make sure the multiplication operation is saturated
-    // 4177920 is 255 * 2^14 which is the current scale factor at this point
-    if (temp > 4177920) {
-        temp = 4177920;
-    }
-    x[5] = (temp >> 14);
-    temp = q[1] * (x[6] << 7);
-    // make sure the multiplication operation is saturated
-    // 4177920 is 255 * 2^14 which is the current scale factor at this point
-    if (temp > 4177920) {
-        temp = 4177920;
-    }
-    x[6] = (temp >> 14);
+    int temp = q[1] * x[5];
+  
+    x[5] = temp;
+    temp = q[1] * x[6];
+
+    x[6] = temp;
+    swap(&x[1], &x[4], &x[7]);
+    swap(&x[3], &x[6], &x[5]);
     
     //print result
     printf("Stage 4:\n\n");
     for (i = 8; i; i--) {
-        printf("x[%d]: %u\n", 8-i, x[8-i]);
+        printf("x[%d]: %d\n", 8-i, x[8-i]);
     }
 
     // keep in mind that after loeffler's algorithm is done, the output array X has shifted:
     // x[0] = X[0], x[1] = X[4], x[2] = X[2], x[3] = X[6], x[4] = X[7], x[5] = X[3], x[6] = X[5], x[7] = X[1] 
+    // now resolved by swap function before stage 4 results are printed
 }
